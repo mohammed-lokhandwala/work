c273acb38e29da55433036fb1192a44693bce012
mohammedlokhandwala <mohammedlokhand2021@gmail.com>
2024-10-08 11:07:25 +0530

    working code

--- App.js
+++ App.js
@@ -4,42 +4,34 @@ import { FlatList, View, StyleSheet, Button, Image, Text } from "react-native";
 
 
 const samplebook = [
-  { id: '1', content: require('./abc2.jpg'), bookTag: 1, bookpage: 1 },
-  { id: '2', content: require('./abc2.jpg'), bookTag: 1, bookpage: 2 },
-  { id: '3', content: require('./abc2.jpg'), bookTag: 1, bookpage: 3 },
-  { id: '4', content: require('./abc2.jpg'), bookTag: 1, bookpage: 4 },
-  { id: '5', content: require('./abc2.jpg'), bookTag: 1, bookpage: 5 },
+  { id: '1', content: require('./1.jpg'), bookTag: 1, bookpage: 1 },
+  { id: '2', content: require('./2.jpg'), bookTag: 1, bookpage: 2 },
+  { id: '3', content: require('./3.jpg'), bookTag: 1, bookpage: 3 },
+  { id: '4', content: require('./4.jpg'), bookTag: 1, bookpage: 4 },
+  { id: '5', content: require('./5.jpg'), bookTag: 1, bookpage: 5 },
 
-  { id: '6', content: require('./abc2.jpg'), bookTag: 1, bookpage: 6 },
-  { id: '7', content: require('./abc2.jpg'), bookTag: 1, bookpage: 7 },
-  { id: '8', content: require('./abc2.jpg'), bookTag: 1, bookpage: 8 },
-  { id: '9', content: require('./abc2.jpg'), bookTag: 1, bookpage: 9 },
-  { id: '10', content: require('./abc2.jpg'), bookTag: 1, bookpage: 10 },
-
-  { id: '11', content: require('./sample.jpg'), bookTag: 2, bookpage: 1 },
-  { id: '12', content: require('./sample.jpg'), bookTag: 2, bookpage: 2 },
-  { id: '13', content: require('./sample.jpg'), bookTag: 2, bookpage: 3 },
-  { id: '14', content: require('./sample.jpg'), bookTag: 2, bookpage: 4 },
-  { id: '15', content: require('./sample.jpg'), bookTag: 2, bookpage: 5 },
-
-  { id: '16', content: require('./sample.jpg'), bookTag: 2, bookpage: 6 },
-  { id: '17', content: require('./sample.jpg'), bookTag: 2, bookpage: 7 },
-  { id: '18', content: require('./sample.jpg'), bookTag: 2, bookpage: 8 },
-
-  { id: '19', content: require('./abc2.jpg'), bookTag: 1, bookpage: 11 },
-  { id: '20', content: require('./abc2.jpg'), bookTag: 1, bookpage: 12 },
-  { id: '21', content: require('./abc2.jpg'), bookTag: 1, bookpage: 13 },
-  { id: '22', content: require('./abc2.jpg'), bookTag: 1, bookpage: 14 },
-  { id: '23', content: require('./abc2.jpg'), bookTag: 1, bookpage: 15 },
+  { id: '6', content: require('./above.jpg'), bookTag: 1, bookpage: 6 },
+  { id: '7', content: require('./above.jpg'), bookTag: 1, bookpage: 7 },
+  { id: '8', content: require('./above.jpg'), bookTag: 1, bookpage: 8 },
+  { id: '9', content: require('./above.jpg'), bookTag: 1, bookpage: 9 },
+  { id: '10', content: require('./above.jpg'), bookTag: 1, bookpage: 10 },
 
 
-  { id: '24', content: require('./abc2.jpg'), bookTag: 1, bookpage: 16 },
-  { id: '25', content: require('./abc2.jpg'), bookTag: 1, bookpage: 17 },
-  { id: '26', content: require('./abc2.jpg'), bookTag: 1, bookpage: 18 },
-  { id: '27', content: require('./abc2.jpg'), bookTag: 1, bookpage: 19 },
-  { id: '28', content: require('./abc2.jpg'), bookTag: 1, bookpage: 20 },
+  { id: '11', content: require('./1.jpg'), bookTag: 1, bookpage: 1 },
+  { id: '12', content: require('./2.jpg'), bookTag: 1, bookpage: 2 },
+  { id: '13', content: require('./3.jpg'), bookTag: 1, bookpage: 3 },
+  { id: '14', content: require('./4.jpg'), bookTag: 1, bookpage: 4 },
+  { id: '15', content: require('./5.jpg'), bookTag: 1, bookpage: 5 },
 
- 
+  { id: '16', content: require('./above.jpg'), bookTag: 1, bookpage: 6 },
+  { id: '17', content: require('./above.jpg'), bookTag: 1, bookpage: 7 },
+  { id: '19', content: require('./above.jpg'), bookTag: 1, bookpage: 8 },
+  { id: '20', content: require('./above.jpg'), bookTag: 1, bookpage: 9 },
+  { id: '21', content: require('./above.jpg'), bookTag: 1, bookpage: 10 },
+
+
+
+
 
 ];
 
@@ -50,7 +42,7 @@ const samplebook = [
 
 
 const mainBook = []
-let testBook = []
+
 var no_of_pages_loaded = 0;
 
 // inital render of every first 5 pages of book
@@ -94,20 +86,21 @@ export default function Home() {
           for (let i =0; i < samplebook.length; i++ ) {
         
         
-            if(samplebook[i].bookpage <= no_of_pages_loaded) {
+            if(samplebook[i].bookpage <= (no_of_pages_loaded + 5) && samplebook[i].bookpage > no_of_pages_loaded) {
 
               mainBook.push({id : samplebook[i].id, content: samplebook[i].content})
             }
           
            } 
         no_of_pages_loaded += 5 
+        console.log(no_of_pages_loaded)
         
         } else {
           
         }
         
       }
-      console.log(testBook,page,no_of_pages_loaded )
+      console.log(page,no_of_pages_loaded )
     }
   };
 
@@ -164,3 +157,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
   }
 });
+
+
+
+
+
+
